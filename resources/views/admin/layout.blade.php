9c42364959a4bf69a416f1e0325585b4c545d3e2
DavarAly <davaralyiskandar18@gmail.com>
2026-07-30 13:24:17 +0700

    Memperbaiki database

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -10,6 +10,7 @@
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 </script>
+<link rel="icon" type="image/png" href="{{ asset('images/favicon-bg.png') }}?v=2">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 <style>
